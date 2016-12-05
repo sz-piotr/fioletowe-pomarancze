1807e4d3289ae316fccd15ae0d6835ccad1f0241
sz-piotr <szlachciak.piotr@gmail.com>
2016-12-05 20:23:17 +0100

    add create sql

---
+++ sql/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE animals
+(
+  id bigserial NOT NULL,
+  name character varying(100) NOT NULL,
+  sound character varying(20),
+  CONSTRAINT animals_pk PRIMARY KEY (id)
+)
+ALTER TABLE animals
+  OWNER TO "user";
