d85bc4fbb5c1a65b73e66b694e02f7799b05ef1d
sz-piotr <szlachciak.piotr@gmail.com>
2016-12-08 12:15:10 +0100

    poprawienie syntax error w sql

--- sql/create.sql
+++ sql/create.sql
@@ -4,6 +4,6 @@ CREATE TABLE animals
   name character varying(100) NOT NULL,
   sound character varying(20),
   CONSTRAINT animals_pk PRIMARY KEY (id)
-)
+);
 ALTER TABLE animals
   OWNER TO "user";
