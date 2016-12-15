000b5f1bdd92a99824d904b99766dbc04446717a
sz-piotr <szlachciak.piotr@gmail.com>
2016-12-16 00:04:15 +0100

    dodanie przykładowego zapytania do backendu

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,3 +1,14 @@
 $(function() {
-    $("#title").html("IT works!")
+    $.getJSON("http://localhost:5000/animals", function(data) {
+        data.forEach(function(animal) {
+            if (animal.sound !== null) {
+                var text = animal.name + " does " + animal.sound;
+            } else {
+                var text = animal.name + " is silent";
+            }
+            $("#animals").append("<li>" + text + "</li>");
+        })
+    }).fail(function() {
+        $("body").append("Something went wrong. Make sure backend is running.")
+    });
 });
