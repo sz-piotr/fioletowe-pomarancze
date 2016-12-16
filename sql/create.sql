a34c4845acc6837be58e1482b6243ca9c3a72bbc
sz-piotr <szlachciak.piotr@gmail.com>
2016-12-16 19:35:48 +0100

    dodanie tabeli userów

--- sql/create.sql
+++ sql/create.sql
@@ -1,9 +1,20 @@
 CREATE TABLE animals
 (
-  id bigserial NOT NULL,
-  name character varying(100) NOT NULL,
-  sound character varying(20),
-  CONSTRAINT animals_pk PRIMARY KEY (id)
+    id bigserial NOT NULL,
+    name character varying(100) NOT NULL,
+    sound character varying(20),
+    CONSTRAINT animals_pk PRIMARY KEY (id)
 );
 ALTER TABLE animals
-  OWNER TO "user";
+    OWNER TO "fioletowe";
+
+CREATE TABLE users
+(
+    id bigserial NOT NULL,
+    username character varying(100) NOT NULL,
+    hash character(88) NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id),
+    CONSTRAINT users_username_un UNIQUE (username)
+);
+ALTER TABLE users
+    OWNER TO "fioletowe";
