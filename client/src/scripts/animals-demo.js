442771e867ab4d1c59f0cb72054acd3afb976e53
sz-piotr <szlachciak.piotr@gmail.com>
2016-12-16 15:29:04 +0100

    stworzenie jako takiej struktury kodu dla klienta

---
+++ client/src/scripts/animals-demo.js
@@ -0,0 +1,16 @@
+function getAnimals() {
+    $.getJSON("http://localhost:5000/animals", function(data) {
+        $("#animals-alert").hide();
+        $("#animals").html("");
+        data.forEach(function(animal) {
+            if (animal.sound !== null) {
+                var text = animal.name + " does " + animal.sound;
+            } else {
+                var text = animal.name + " is silent";
+            }
+            $("#animals").append("<li>" + text + "</li>");
+        })
+    }).fail(function() {
+        $("#animals-alert").show();
+    });
+}
--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,14 +1,8 @@
-function getAnimals() {
-    $.getJSON("http://localhost:5000/animals", function(data) {
-        $("#animals-alert").hide();
-        $("#animals").html("");
-        data.forEach(function(animal) {
-            if (animal.sound !== null) {
-                var text = animal.name + " does " + animal.sound;
-            } else {
-                var text = animal.name + " is silent";
-            }
-            $("#animals").append("<li>" + text + "</li>");
-        })
+$(".sidebar-nav").children().not(".sidebar-brand").each(function() {
+    var link = $(this).find('a');
+    link.click(function() {
+        $(".sidebar-nav").children().find('a').removeClass('active');
+        console.log(link)
+        link.addClass('active');
     });
-}
+});
