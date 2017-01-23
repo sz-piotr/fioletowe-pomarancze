a3bd0e9226d878bd18ed5150ca83210dd7fb250a
sz-piotr <szlachciak.piotr@gmail.com>
2017-01-23 12:12:35 +0100

    fix: sometimes login failed

--- server/src/auth/views.py
+++ server/src/auth/views.py
@@ -3,18 +3,19 @@ from users.models import User
 from flask import jsonify, request
 from exceptions import LoginError
 from util.decorators import request_schema
+from sqlalchemy import or_
 
 
 @auth.route('/login', methods=['POST'])
 @request_schema(schemas.login)
 def login():
     data = request.get_json(force=True)
-    user = User.query.filter_by(username=data['username']).first()
-    if user == None:
-        user = User.query.filter_by(email=data['username']).first()
-    if user != None and user.check(data['password']):
-        return jsonify({
-            'token': tokens.encode(user).decode('utf-8')
-        })
+    users = User.query.filter(or_(User.username==data['username'], User.email==data['username'])).all()
+    for user in users:
+        if user.check(data['password']):
+            return jsonify({
+                'token': tokens.encode(user).decode('utf-8'),
+                'username': user.username
+            })
     else:
         raise LoginError()
