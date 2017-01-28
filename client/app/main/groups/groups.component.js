c7c5cce3d4aabc89e8a2803334ec78808b96992b
sz-piotr <szlachciak.piotr@gmail.com>
2017-01-28 20:41:13 +0100

    dodanie wyjścia z modali, walidacja form w modalach

--- client/app/main/groups/groups.component.js
+++ client/app/main/groups/groups.component.js
@@ -3,7 +3,7 @@ angular
     .component('groups', {
         templateUrl: 'main/groups/groups.html',
         css: 'main/groups/groups.css',
-        controller: function GroupsController(GroupsService, SharesService) {
+        controller: function GroupsController(GroupsService, SharesService, $scope) {
             this.update = () => {
                 this.loadingGroups = true;
                 this.groupsError = false;
@@ -18,14 +18,31 @@ angular
             this.update();
 
             this.openAddGroupModal = () => {
+                delete this.group;
                 this.addGroupError = false;
+                $scope.addgroupform.$setPristine();
+                $scope.addgroupform.$setUntouched();
                 $('#add-group-modal').modal('show');
             }
 
+            this.addGroup = () => {
+                $scope.addgroupform.$submitted = true;
+                if (!$scope.addgroupform.$invalid) {
+                    console.log('Group', this.group);
+                    this.submitting = true;
+                    this.addGroupError = "Adding groups is not implemented";
+                    // TODO add group here! GroupService.addGroup
+                    this.submitting = false;
+                }
+            }
+
             this.openAddShareModal = group => {
+                delete this.share;
                 this.selectedGroup = group;
-                this.loadingShares = true;
                 this.addShareError = false;
+                $scope.addshareform.$setPristine();
+                $scope.addshareform.$setUntouched();
+                this.loadingShares = true;
                 SharesService.query()
                     .then(response => {
                         this.shares = [];
@@ -38,10 +55,37 @@ angular
                 $('#add-share-modal').modal('show');
             }
 
+            this.addShare = () => {
+                $scope.addshareform.$submitted = true;
+                if (!$scope.addshareform.$invalid) {
+                    console.log('Selected Group', this.selectedGroup);
+                    console.log('Share', this.share);
+                    this.submitting = true;
+                    this.addShareError = "Adding shares is not implemented";
+                    // TODO add share here! GroupService.addShare
+                    this.submitting = false;
+                }
+            }
+
             this.openAddMemberModal = group => {
+                delete this.member;
                 this.selectedGroup = group;
                 this.addMemberError = false;
+                $scope.addmemberform.$setPristine();
+                $scope.addmemberform.$setUntouched();
                 $('#add-member-modal').modal('show');
+            }
+
+            this.addMember = () => {
+                $scope.addmemberform.$submitted = true;
+                if (!$scope.addmemberform.$invalid) {
+                    console.log('Selected Group', this.selectedGroup);
+                    console.log('Member', this.member);
+                    this.submitting = true;
+                    this.addMemberError = "Adding members is not implemented";
+                    // TODO add share here! GroupService.addMember
+                    this.submitting = false;
+                }
             }
 
             this.openRemoveGroupModal = group => {
