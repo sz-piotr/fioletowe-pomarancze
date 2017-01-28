810d7558596444a555b7182a068cb28db06d38ee
Lidka <lidkabondarzewska@gmail.com>
2017-01-28 16:31:24 +0100

    devices - dodanie sprawdzenia błędów

--- server/src/devices/views.py
+++ server/src/devices/views.py
@@ -12,7 +12,6 @@ from util.decorators import request_schema
 @devices.route('/devices', methods=['GET'])
 @login_required
 def list():
-    # TODO check for errors
     devices = Device.query.filter_by(user_id=g.auth['sub']).all()
     return jsonify({
         'devices': Device.serialize_list(devices)
@@ -23,26 +22,22 @@ def list():
 @login_required
 @request_schema(schemas.add_device)
 def add(device_name):
-    # TODO check for errors
-    device = Device(
-        name=device_name,
-        address=g.data['address'],
-        user_id=g.auth['sub']
-    )
-    db.session.add(device)
-    db.session.commit()
-
-
-
+    try:
+        device = Device(
+            name=device_name,
+            address=g.data['address'],
+            user_id=g.auth['sub']
+        )
+        db.session.add(device)
+        db.session.commit()
+    except IntegrityError as e:
+        raise AlreadyExistsError(device_name)
     return ('', HTTPStatus.NO_CONTENT)
 
 
 @devices.route('/devices/<device_name>', methods=['DELETE'])
 @login_required
 def remove(device_name):
-    # TODO check for errors
     Device.query.filter_by(user_id=g.auth['sub'], name=device_name).delete()
-
     db.session.commit()
-
     return ('', HTTPStatus.NO_CONTENT)
