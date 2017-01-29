d689f9cdf54086dddf7b25da900acd2fa6837b24
lapsio <lapsio3@gmail.com>
2017-01-29 04:39:59 +0100

    shares requests

--- client/app/main/shares/shares.service.js
+++ client/app/main/shares/shares.service.js
@@ -12,20 +12,131 @@ angular
             }
         }
 
+        function extractError(error){
+            if (!(error.data && error.data.msg))
+                return error.statusText || 'Unknown error';
+            else
+                return error.data.msg;
+        }
+
         return {
             query() {
                 return $q(function (resolve, reject) {
-                    $http.get('main/shares/fake-devices.json')
+                    $http.get('/api/devices')
                         .then(response => {
+                            console.log(response);
                             var devices = response.data.devices;
-                            devices.forEach(x => x.shares = []);
-                            $http.get('main/shares/fake-shares.json')
+                            devices.forEach(x => {
+                                x.shares = [];
+                                x.name=decodeURIComponent(x.name);
+                                x.address=decodeURIComponent(x.address);
+                            });
+                            $http.get('/api/shares')
                                 .then(response => {
+                                    console.log(response);
                                     var shares = response.data.shares;
-                                    shares.forEach(share => findByName(devices, share.device).shares.push(share));
+                                    shares.forEach(share => {
+                                        share.name=decodeURIComponent(share.name);
+                                        share.device=decodeURIComponent(share.device);
+                                        share.paths.forEach(p => {
+                                            p.name=decodeURIComponent(p.name);
+                                            p.path=decodeURIComponent(p.path);
+                                        });
+                                        findByName(devices, share.device).shares.push(share)
+                                    });
                                     resolve(devices);
-                                }, error => reject(error));
-                        }, error => reject(error));
+                                }, error => reject(extractError(error)));
+                        }, error => {
+                            console.error('list',error);
+                            reject(extractError(error));
+                        });
+                });
+            },
+            deleteDevice(name) {
+                name = encodeURIComponent(name);
+                return $q(function (resolve, reject) {
+                    $http.delete(`/api/devices/${name}`)
+                        .then(res => {
+                            console.log('ok',name);
+                            resolve(res);
+                        }, error => {
+                            console.error(name,error);
+                            reject(extractError(error));
+                        });
+                });
+            },
+            deleteShare(name) {
+                name = encodeURIComponent(name);
+                return $q(function (resolve, reject) {
+                    $http.delete(`/api/shares/${name}`)
+                        .then(res => {
+                            console.log('ok',name);
+                            resolve(res);
+                        }, error => {
+                            console.error(name,error);
+                            reject(extractError(error))
+                        });
+                });
+            },
+            deletePath(path,share) {
+                path = encodeURIComponent(path);
+                share = encodeURIComponent(share);
+                return $q(function (resolve, reject) {
+                    $http.delete(`/api/shares/${share}/paths/${path}`)
+                        .then(res => {
+                            console.log('ok',share,path);
+                            resolve(res);
+                        }, error => {
+                            console.error(share,path,error);
+                            reject(extractError(error));
+                        });
+                });
+            },
+
+            addDevice(name,addr) {
+                name = encodeURIComponent(name);
+                addr = encodeURIComponent(addr);
+                return $q(function (resolve, reject) {
+                    $http.post(`/api/devices/${name}`,{
+                        address:addr
+                    }).then(res => {
+                        console.log('ok',name);
+                        resolve(res);
+                    }, error => {
+                        console.error(name,error);
+                        reject(extractError(error));
+                    });
+                });
+            },
+            addShare(name,dev) {
+                name = encodeURIComponent(name);
+                dev = encodeURIComponent(dev);
+                return $q(function (resolve, reject) {
+                    $http.post(`/api/shares/${name}`,{
+                        device:dev
+                    }).then(res => {
+                        console.log('ok',name);
+                        resolve(res);
+                    }, error => {
+                        console.error(name,error);
+                        reject(extractError(error));
+                    });
+                });
+            },
+            addPath(path,share,syspath) {
+                path = encodeURIComponent(path);
+                share = encodeURIComponent(share);
+                syspath = encodeURIComponent(syspath);
+                return $q(function (resolve,reject) {
+                    $http.post(`/api/shares/${share}/paths/${path}`,{
+                        path:syspath
+                    }).then(res => {
+                        console.log('ok',share,path);
+                        resolve(res);
+                    }, error => {
+                        console.error(share,path,error);
+                        reject(extractError(error));
+                    });
                 });
             }
         }
