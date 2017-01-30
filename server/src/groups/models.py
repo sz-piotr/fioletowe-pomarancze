4c6892cba418c15cd0575a38a695a64e62624654
sz-piotr <szlachciak.piotr@gmail.com>
2017-01-30 21:18:34 +0100

    fix: cannot remove device

--- server/src/groups/models.py
+++ server/src/groups/models.py
@@ -3,7 +3,7 @@ from serializer import Serializer
 from users.models import user_group_association
 
 share_group_association = db.Table('share_group_association', db.Model.metadata,
-    db.Column('share_id', db.Integer, db.ForeignKey('share.id'), nullable=False),
+    db.Column('share_id', db.Integer, db.ForeignKey('share.id', ondelete="CASCADE"), nullable=False),
     db.Column('group_id', db.Integer, db.ForeignKey('group.id', ondelete="CASCADE"), nullable=False),
     db.UniqueConstraint('share_id', 'group_id', name='share_group_association_uc')
 )
@@ -19,7 +19,7 @@ class Group(db.Model, Serializer):
     user = db.relationship("User", back_populates="own_groups")
 
     members = db.relationship("User", secondary=user_group_association, back_populates="in_groups")
-	
+
     shares = db.relationship("Share", secondary=share_group_association, back_populates="groups")
 
     __table_args__ = (db.UniqueConstraint('name', 'user_id', name='group_name_user_id_uc'), )
