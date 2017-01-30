e2f121ea04bd95aeb954976512e99389b33c1b16
Sylwia Blach <sylwia.blach404@gmail.com>
2017-01-30 16:27:57 +0100

    fix: list avaliable shares - only from in_groups

--- server/src/shares/views/public.py
+++ server/src/shares/views/public.py
@@ -5,6 +5,7 @@ from auth.decorators import login_required
 from util.decorators import request_schema
 from shares.models import Share
 from users.models import User
+from exceptions import DoesNotExistError
 from shares.views.utils import out_aval_share, out_path
 from auth.tokens import encode_access, decode
 
@@ -12,12 +13,17 @@ from auth.tokens import encode_access, decode
 @shares.route('/shares/public', methods=['GET'])
 @login_required
 def list_shares():
-    shares = Share.query.all()
-    results = [out_aval_share(share) for share in shares]
+    user = User.query.filter_by(id = g.auth['sub']).first()
+    if (user is None):
+        raise DoesNotExistError(g.data['user'])
+	
+    results = []
+    for group in user.in_groups:
+        results.extend([out_aval_share(share) for share in group.shares])
     return jsonify({
         'shares':
             results
-    })
+    })	
 
 
 @shares.route('/shares/public/<user_name>/at', methods=['GET'])
@@ -31,15 +37,15 @@ def get_access_token(user_name):
     shares = [out_aval_share(share) for share in shares]
     return jsonify({
         'token': token.decode('utf-8'),
-        'shares':
+        'shares': 
             shares
     })
 
 
-@shares.route('/shares/public/verify/at', methods=['POST'])
+@shares.route('/shares/public/verify/at', methods=['GET'])
 @login_required
 @request_schema(schemas.verify_access_token)
-def verify_access_token():
+def verify_access_token(user_name):
     user_name = 'user1'
     json_request = request.get_json()
     path = json_request['path']
@@ -49,7 +55,7 @@ def verify_access_token():
     if user == None:
         raise DoesNotExistError(user_name)
     token = encode_access(user)
-
+    
     return jsonify({
         'allowed': True if token is verified_token else False,
         'realpath': '/real/' + path
